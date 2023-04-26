35bc2737172bc8728b31a678abf8b7f8584fd263
Anasa <hamidlmath534@gmail.com>
2023-04-26 10:18:24 +0100

    finalVersion

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi i'm Imane!");
+        System.out.println("Fine and you ?");
     }
 }
